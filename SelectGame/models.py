e48b930c36de008f627bc7de766db17d9d67419c
Narius <broberg.marcus@gmail.com>
2018-02-12 16:02:27 +0100

    resolves #2

--- SelectGame/models.py
+++ SelectGame/models.py
@@ -3,7 +3,16 @@ from django.db import models
 # Create your models here.
 
 #Model for categories
+class model_category(models.Model):
+    name = models.CharField(max_length=30,
+                            verbose_name="Name")
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+    def __str__(self):
+        return self.name
 
 #Model for games
+#toppings = models.ManyToManyField(Topping)
 
 #Model for ratings
