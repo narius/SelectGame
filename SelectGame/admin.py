6a3030e83006dcffaf8bb22c86c057d041027da9
Narius <broberg.marcus@gmail.com>
2018-02-12 16:11:01 +0100

    Resolves #1

--- SelectGame/admin.py
+++ SelectGame/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from  .models import model_category
+from  .models import model_category, model_game
 # Register your models here.
 admin.site.register(model_category)
+admin.site.register(model_game)
--- SelectGame/models.py
+++ SelectGame/models.py
@@ -13,6 +13,15 @@ class model_category(models.Model):
         return self.name
 
 #Model for games
+class model_game(models.Model):
+    name=models.CharField(max_length=30, verbose_name="Name")
+    category=models.ManyToManyField(model_category)
+    comment=models.TextField(verbose_name="Comment")
+    class Meta:
+        verbose_name = "game"
+        verbose_name_plural = "games"
+    def __str__(self):
+        return self.name
 #toppings = models.ManyToManyField(Topping)
 
 #Model for ratings
