db52c9eda767a402e719a2e91f03c96356c157fc
Marcus Broberg <broberg.marcus@gmail.com>
2018-02-14 19:28:49 +0100

    resolves #6

--- SelectGame/admin.py
+++ SelectGame/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from  .models import model_category, model_game
+from  .models import model_category, model_game, model_location
 # Register your models here.
 admin.site.register(model_category)
 admin.site.register(model_game)
+admin.site.register(model_location)
--- SelectGame/models.py
+++ SelectGame/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils.translation import gettext
+from  django.contrib.auth.models import User
 # Create your models here.
 
 #Model for categories
@@ -18,10 +19,20 @@ class model_game(models.Model):
     category=models.ManyToManyField(model_category)
     comment=models.TextField(verbose_name="Comment")
     class Meta:
-        verbose_name = "game"
-        verbose_name_plural = "games"
+        verbose_name = gettext("game")
+        verbose_name_plural = gettext("games")
     def __str__(self):
         return self.name
 #toppings = models.ManyToManyField(Topping)
 
 #Model for ratings
+
+#Model for location
+class model_location(models.Model):
+    address=models.CharField(max_length=100, verbose_name=gettext("address"))
+    owner = models.ForeignKey(User)
+    class Meta:
+        verbose_name=gettext("Address")
+        verbose_name_plural=gettext("Address")
+    def __str__(self):
+        return self.owner+"-"+self.address
