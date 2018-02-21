517f93aaa11bfb47d789de537fc173583d103cea
Narius <broberg.marcus@gmail.com>
2018-02-21 21:13:29 +0100

    .

--- Sites/settings.py
+++ Sites/settings.py
@@ -136,8 +136,8 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Update database configuration with $DATABASE_URL.
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Configure Django App for Heroku.
 import django_heroku
