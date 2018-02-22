b0f8ee2cc801acfeec956f8207466e1bd6761e0f
Marcus Broberg <broberg.marcus@gmail.com>
2018-02-22 19:00:30 +0100

    d

--- Sites/settings.py
+++ Sites/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         default=dj_database_url.config('DATABASE_URL')
     )
 }
-
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 # Update database configuration with $DATABASE_URL.
 #db_from_env = dj_database_url.config(conn_max_age=500)
 #DATABASES['default'].update(db_from_env)
