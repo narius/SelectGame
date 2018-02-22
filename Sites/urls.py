0df2630ce13e3a076becfa69eb4cd4f032c3f9e9
Narius <broberg.marcus@gmail.com>
2018-02-22 19:39:38 +0100

    Fixing base url

--- Sites/urls.py
+++ Sites/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('selectgame/', include('SelectGame.urls')),
-    path('/', include('SelectGame.urls')),
+    path('', include('SelectGame.urls')),
 ]
