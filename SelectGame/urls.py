705110cd113a71d5b1747ef5ac967fa7b8a0541d
Narius <broberg.marcus@gmail.com>
2018-02-23 21:40:51 +0100

    View for showing all ratings

--- SelectGame/urls.py
+++ SelectGame/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'selectgame'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ratings/',views.all_game_rating,name='all_game_rating'),
 ]
--- SelectGame/views.py
+++ SelectGame/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import model_rating
 def index(request):
     return render(request, 'SelectGame/index.html')
+
+def all_game_rating(request):
+    ratings=model_rating.objects.all()
+    return render(request, 'SelectGame/all_game_rating.html',{'ratings':ratings,})
