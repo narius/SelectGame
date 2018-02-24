de7a67841708e4781ec65292cc3369c3b4b375a5
Narius <broberg.marcus@gmail.com>
2018-02-24 07:57:00 +0100

    added login and logout functionality

--- SelectGame/urls.py
+++ SelectGame/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
 from . import views
 app_name = 'selectgame'
 urlpatterns = [
     path('', views.index, name='index'),
     path('ratings/',views.all_game_rating,name='all_game_rating'),
+    path('login/', login, name='login'),
+    path('logout/', logout, {'next_page': 'selectgame:index'},name='logout'),
 ]
--- SelectGame/views.py
+++ SelectGame/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import model_rating
 from .models import model_game
 from .rating import rating_functions
+from django.contrib.auth import logout
 def index(request):
     return render(request, 'SelectGame/index.html')
 
@@ -15,3 +16,8 @@ def all_game_rating(request):
     for mean_r in mean_rating_per_game:
         print(mean_r)
     return render(request, 'SelectGame/all_game_rating.html',{'ratings':mean_rating_per_game,})
+
+def logout_view(request):
+    logout(request)
+    # Redirect to a success page.
+    return render(request,'registration/logged_out.html')
