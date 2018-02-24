50357dccf864c571368c54d10db320bf11745d8e
Narius <broberg.marcus@gmail.com>
2018-02-24 19:08:46 +0100

    Started to work on a test suit

--- SelectGame/tests.py
+++ SelectGame/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from SelectGame.models import model_rating
+from SelectGame.models import model_game
+from django.contrib.auth.models import User
+from .rating import rating_functions
+from django.contrib.auth.signals import user_logged_out
+from django.contrib.auth.signals import user_logged_in
+from django.db.models import signals
+class RatingTestCase(TestCase):
+    def setUp(self):
+        print("Hi from RatingTestCase")
+        user_logged_in.disconnect()
+        self.game=model_game.objects.create(name="TestGame")
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
+        self.user2 = User.objects.create_user(username='testuser2', password='12345')
+        login = self.client.login(username='testuser2', password='12345')
+        rating.object.create(game=self.game, user=self.user,rating=4)
+        rating.object.create(game=self.game, user=self.user2,rating=5)
 
+    def test_rating_per_game(self):
+        self.assertEqual(rating_functions(self.game), 4.5)
 # Create your tests here.
--- Sites/settings.py
+++ Sites/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
 ROOT_URLCONF = 'Sites.urls'
