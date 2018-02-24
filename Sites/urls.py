8fe9387135e4f41bab29632d4f0e81a5c8b1c36f
Narius <broberg.marcus@gmail.com>
2018-02-24 19:18:23 +0100

    Avoiding ?: (urls.W005) URL namespace 'selectgame' isn't unique. You may not be able to r

--- Sites/urls.py
+++ Sites/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('selectgame/', include('SelectGame.urls')),
     path('', include('SelectGame.urls')),
 ]
