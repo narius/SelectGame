2507870f5e0530af36a8f28b486e8a835c9f7a67
Narius <broberg.marcus@gmail.com>
2018-02-25 14:56:42 +0100

    Made views to profiles

--- Social/urls.py
+++ Social/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'social'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile/<int:user_id>/', views.display_profile, name='user_profile'),
+    path('profile/', views.display_profiles, name='profiles')
 ]
--- Social/views.py
+++ Social/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.contrib.auth.models import User
+from Social.models import model_user_profile
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the social index.")
+
+def display_profile(request, user_id):
+    user=User.objects.get(pk=user_id)
+    profile=model_user_profile.objects.get(user=user)
+    return render(request, 'Social/profile.html',{'user':user,
+                                                    'profile':profile,})
+def display_profiles(request):
+    users=User.objects.all()
+    if request.method=="POST":
+        user_id=request.POST.get("user")
+        print("user id: "+str(user_id))
+        user=User.objects.get(pk=user_id)
+        try:
+            profile=model_user_profile.objects.get(user=user)
+        except ObjectDoesNotExist:
+            profile={}
+        return render(request, 'Social/profile.html',{'users':users,
+                                                        'user':user,
+                                                        'profile':profile,})
+    else:
+        return render(request, 'Social/profile.html',{'users':users,})
