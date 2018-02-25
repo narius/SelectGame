aab7bd3a0bb6754443e412affbc5274d12733f0d
Narius <broberg.marcus@gmail.com>
2018-02-25 11:47:14 +0100

    added friends and private message models, resolves #19

--- Social/models.py
+++ Social/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class model_group(models.Model):
     name=models.CharField(max_length=30, verbose_name=gettext('name'))
     owners=models.ManyToManyField(User,verbose_name=gettext('owners'), related_name="group_owner")
-    members=models.ManyToManyField(User, verbose_name=gettext('participants'), related_name="group_members")
+    members=models.ManyToManyField(User, verbose_name=gettext('members'), related_name="group_members")
     class Meta:
         verbose_name=gettext('group')
         verbose_name_plural=gettext('groups')
@@ -39,9 +39,34 @@ class model_user_profile(models.Model):
     def __str__(self):
         return str(self.user)
 
+#Status for friends
+friends_pending='pe'
+friends_accepted='ac'
+friends_rejected='re'
+friends_removed='rm'
+FRIENDS_STATUS=(
+    (friends_pending,gettext('pending approval')),
+    (friends_accepted,gettext('accepted')),
+    (friends_rejected,gettext('rejected')),
+    (friends_removed,gettext('removed')),
+)
+class model_friends(models.Model):
+    status_user1=models.CharField(max_length=2,choices=FRIENDS_STATUS,default=friends_pending)
+    status_user2=models.CharField(max_length=2,choices=FRIENDS_STATUS,default=friends_pending)
+    user1=models.ForeignKey(User, related_name="User1", on_delete=models.CASCADE)
+    user2=models.ForeignKey(User, related_name="User2", on_delete=models.CASCADE)
+    class Meta:
+        verbose_name=gettext("friend")
+        verbose_name_plural=gettext("friends")
+        unique_together = (("user1", "user2"),)
+    def __str__(self):
+        return str(self.user1)+"-"+str(self.user2)
 
-#TODO: model for friends
-
-#TODO: model for friend requests
-
-#TODO: model for private messages
+class model_private_message(models.Model):
+    participants=models.ManyToManyField(User, verbose_name=gettext('participants'), related_name="participants")
+    messages=models.ManyToManyField(model_message)
+    class Meta:
+        verbose_name=gettext("private message")
+        verbose_name_plural=gettext("private messages")
+    def __str__(self):
+        return str(participants)
