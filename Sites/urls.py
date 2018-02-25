f52c6e95c313a0aa8c4049023de2d4eaa7349452
Narius <broberg.marcus@gmail.com>
2018-02-25 17:30:54 +0100

    Added django-richtextfield

--- Sites/urls.py
+++ Sites/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
+    path('djrichtextfield/', include('djrichtextfield.urls'))
     path('', include('SelectGame.urls')),
     path('social/', include('Social.urls')),
 ]
