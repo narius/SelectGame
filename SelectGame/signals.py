268d60db5db3fec4b35a70cb3e60ae0fda5d9da7
Narius <broberg.marcus@gmail.com>
2018-03-03 09:21:39 +0100

    Made it possible to run test

--- SelectGame/signals.py
+++ SelectGame/signals.py
@@ -7,8 +7,8 @@ from django.utils.translation import gettext
 
 @receiver(user_logged_in)
 def on_user_logged_in(sender, request, **kwargs):
-    messages.add_message(request, messages.SUCCESS, gettext('Logged in.'))
+    messages.add_message(request, messages.SUCCESS, gettext('Logged in.'), fail_silently=True,)
 
 @receiver(user_logged_out)
 def on_user_logged_out(sender, request, **kwargs):
-    messages.add_message(request, messages.SUCCESS, gettext('Logged out.'))
+    messages.add_message(request, messages.SUCCESS, gettext('Logged out.'), fail_silently=True,)
