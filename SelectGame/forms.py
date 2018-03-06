09198859a0c3de0745cfc9710632f26ed23f5529
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-06 15:24:18 +0100

    made the code nicer visually

--- SelectGame/forms.py
+++ SelectGame/forms.py
@@ -3,9 +3,12 @@ from django.forms import ModelForm
 from django.utils.translation import gettext
 from SelectGame.models import model_game, model_category
 
+
 class add_game_form(ModelForm):
     name = forms.CharField(max_length=100)
-    category = forms.ModelMultipleChoiceField(queryset=model_category.objects.all())
+    category = forms.ModelMultipleChoiceField(
+                queryset=model_category.objects.all())
+
     class Meta:
-        model=model_game
-        fields=['name','category']
+        model = model_game
+        fields = ['name', 'category']
