994df8edbd63a0348e5550a31a84ad48ab9e56c9
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-11 10:22:16 +0100

    removed unused settings

--- SelectGame/models.py
+++ SelectGame/models.py
@@ -64,14 +64,14 @@ class model_location(models.Model):
 #Model for an event.
 class model_event(models.Model):
     owner=models.ForeignKey(User, on_delete=models.CASCADE, related_name="event_owner")
-    participants=models.ManyToManyField(User, verbose_name=gettext("participants"), related_name="event_participants", blank=True, null=True)
+    participants=models.ManyToManyField(User, verbose_name=gettext("participants"), related_name="event_participants", blank=True)
     date=models.DateTimeField(verbose_name=gettext("date"))
     games=models.ManyToManyField(model_game, verbose_name=gettext("games"), blank=True)
     location=models.ForeignKey(model_location, on_delete=models.CASCADE, verbose_name=gettext("location"))
     is_public=models.BooleanField(verbose_name=gettext("public"))
     group=models.ForeignKey(model_group, blank=True, on_delete=models.CASCADE, null=True)
     name=models.CharField(max_length=100, verbose_name=gettext("name"), blank=True)
-    messages = models.ManyToManyField(model_message, null=True, blank=True)
+    messages = models.ManyToManyField(model_message, blank=True)
 
     class Meta:
         verbose_name=gettext("event")
