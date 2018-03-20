885ddfb8256a235b05441811745194b8aa26b75d
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-20 20:44:40 +0100

    Confirmes to style guidelines

--- SelectGame/forms.py
+++ SelectGame/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.forms import ModelForm
-from django.utils.translation import gettext
 from SelectGame.models import Game, Category
 
 
@@ -8,7 +7,7 @@ class add_game_form(ModelForm):
     name = forms.CharField(max_length=100)
     category = forms.ModelMultipleChoiceField(
                 queryset=Category.objects.all())
-    #image = forms.ImageField(required=False)
+    # image = forms.ImageField(required=False)
     comment = forms.CharField(required=False)
     minimum_number_of_players = forms.IntegerField()
     maximum_number_of_players = forms.IntegerField()
@@ -17,8 +16,8 @@ class add_game_form(ModelForm):
         model = Game
         fields = ['name', 'category']
         fields = ['name',
-                'category',
-                #'image',
-                'comment',
-                'maximum_number_of_players',
-                'minimum_number_of_players']
+                  'category',
+                  # 'image',
+                  'comment',
+                  'maximum_number_of_players',
+                  'minimum_number_of_players']
--- SelectGame/rating.py
+++ SelectGame/rating.py
@@ -1,38 +1,41 @@
-##a file for function around ratings
-import numpy
+# a file for function around ratings
+
 from .models import Rating
-from .models import Game
-from .models import  GameLibrary
+from .models import GameLibrary
+
+
 class rating_functions():
-    #Function to calulate the average for games
+    # Function to calulate the average for games
     def mean_rating_per_game(games):
-        mean_rating_per_game=[]
+        mean_rating_per_game = []
         for game in games:
-            ratings=Rating.objects.filter(game=game).values('rating')
-            total=len(ratings)
-            votes=[0,0,0,0,0]
-            width=[0,0,0,0,0]
-            if (total!=0):
+            ratings = Rating.objects.filter(game=game).values('rating')
+            total = len(ratings)
+            votes = [0, 0, 0, 0, 0]
+            width = [0, 0, 0, 0, 0]
+            if (total != 0):
                 for rating in ratings:
                     print(rating['rating'])
-                    votes[rating['rating']-1]=votes[rating['rating']-1]+1
-                n=0
-                while n<5:
-                    width[n]=int(round((votes[n]/total)*100))
-                    n=n+1
-                mean_rating=sum(rating['rating'] for rating in ratings) /total
+                    votes[rating['rating']-1] = votes[rating['rating']-1]+1
+                n = 0
+                while n < 5:
+                    width[n] = int(round((votes[n]/total)*100))
+                    n = n+1
+                mean_rating = sum(rating['rating']
+                                  for rating in ratings) / total
             else:
-                mean_rating=0
-            mean_rating_per_game.append({'game':game,
-                                        'mean_rating':mean_rating,
-                                        'total':total,
-                                        'votes':votes,
-                                        'width':width})
+                mean_rating = 0
+            mean_rating_per_game.append({'game': game,
+                                         'mean_rating': mean_rating,
+                                         'total': total,
+                                         'votes': votes,
+                                         'width': width})
         return mean_rating_per_game
 
     def users_rating(users, lower_limit):
         """
-            This function will return the average rating for all games that :model:`auth.User` have
+            This function will return the average rating for all games that
+            :model:`auth.User` have
             rated higher than lower_limit
         """
 
@@ -41,30 +44,36 @@ class rating_functions():
         number_of_votes = {}
         avarage = []
         for user in users:
-            #Retrieve all games that the users own.
-            GameLibrary = GameLibrary.objects.get_or_create(owner=user)[0]
-            for game in GameLibrary.games.all():
-                user_ratings = Rating.objects.all().filter(user=user).filter(game=game)
+            # Retrieve all games that the users own.
+            users_game_library = \
+                GameLibrary.objects.get_or_create(owner=user)[0]
+            for game in users_game_library.games.all():
+                user_ratings = \
+                    Rating.objects.all().filter(user=user).filter(game=game)
                 for rating in user_ratings:
                     if rating.rating > lower_limit:
                         print(rating.game.name)
-                        ratings[rating.game.name] = ratings.setdefault(rating.game.name,0)+rating.rating
-                        number_of_votes[rating.game.name] = number_of_votes.setdefault(rating.game.name,0)+1
+                        ratings[rating.game.name] = \
+                            ratings.setdefault(rating.game.name, 0)\
+                            + rating.rating
+                        number_of_votes[rating.game.name] = \
+                            number_of_votes.setdefault(rating.game.name, 0)+1
                 if game not in games:
                     print("rating-append: "+str(game))
                     games.append(game)
-        #By now whe should have two list, one with all available game,
-        #one with all ratings.
+        # By now whe should have two list, one with all available game,
+        # one with all ratings.
 
         print(ratings)
         for game in games:
-            #Find all ratings for this game
-            game_avarage = ratings.setdefault(game.name,0)/number_of_votes.setdefault(game.name,1)
-            votes=number_of_votes[game.name]
-            too_low=True if game_avarage==0 else False
-            avarage.append({'game':game,
-                            'number_of_votes':votes,
+            # Find all ratings for this game
+            game_avarage = ratings.setdefault(game.name, 0) \
+                           / number_of_votes.setdefault(game.name, 1)
+            votes = number_of_votes[game.name]
+            too_low = True if game_avarage == 0 else False
+            avarage.append({'game': game,
+                            'number_of_votes': votes,
                             'average': game_avarage,
                             'too_low': too_low
-            })
+                            })
         return avarage
