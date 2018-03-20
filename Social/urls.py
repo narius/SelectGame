2e1ff277391e3aae3ba2ba05aaf737e3e1748e41
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-20 20:17:12 +0100

    Made code confirme to style guides

--- Social/urls.py
+++ Social/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from django.contrib.auth.views import login
-from django.contrib.auth.views import logout
+
 from . import views
 app_name = 'social'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profile/<int:user_id>/', views.display_profiles, name='user_profile'),
-    path('profile/', views.display_profiles, name='profiles'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('event/<int:event_id>/', views.view_event, name='view_event')
+    path('profile/<int:user_id>/',
+         views.display_profiles,
+         name='user_profile'),
+    path('profile/',
+         views.display_profiles,
+         name='profiles'),
+    path('edit_profile/',
+         views.edit_profile,
+         name='edit_profile'),
+    path('event/<int:event_id>/',
+         views.view_event,
+         name='view_event')
 ]
