a2ce990837a126c11471f69615e264fbfda42d84
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-20 20:35:40 +0100

    removed model_friends

--- Social/admin.py
+++ Social/admin.py
@@ -3,12 +3,10 @@ from Social.models import UserProfile
 from Social.models import Group
 from Social.models import UserMessage
 from Social.models import GroupMessage
-from Social.models import model_friends
 from Social.models import PrivateMessage
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Group)
 admin.site.register(UserMessage)
 admin.site.register(GroupMessage)
-admin.site.register(model_friends)
 admin.site.register(PrivateMessage)
