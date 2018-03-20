33e503151bbf4e4cd173c9b36a8e408b42b802f8
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-20 20:15:52 +0100

    Made the code confirm to stye guidlines

--- Social/views.py
+++ Social/views.py
@@ -12,44 +12,50 @@ from django.utils.translation import gettext
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 # Create your views here.
+
 def index(request):
     return HttpResponse("Hello, world. You're at the social index.")
 
+
 def display_profiles(request, user_id=-1):
-    users=User.objects.all()
-    if request.method=="POST":
-        to_display_profile=True
-        user_id=request.POST.get("user")
+    users = User.objects.all()
+    if request.method == "POST":
+        to_display_profile = True
+        user_id = request.POST.get("user")
         print("user id: "+str(user_id))
-        user=User.objects.get(pk=user_id)
+        user = User.objects.get(pk=user_id)
     else:
         try:
-            user=User.objects.get(pk=user_id) if int(user_id)>-1 else -1
+            user = User.objects.get(pk=user_id) if int(user_id) > -1 else -1
         except ObjectDoesNotExist:
-            user={}
-            messages.add_message(request, messages.ERROR, gettext('User doesn\'t exist'))
+            user = {}
+            messages.add_message(request,
+                                 messages.ERROR,
+                                 gettext('User doesn\'t exist'))
     try:
-        to_display_profile=True
-        profile=UserProfile.objects.get(user=user)
+        to_display_profile = True
+        profile = UserProfile.objects.get(user=user)
     except:
-        to_display_profile=False
-        profile={}
+        to_display_profile = False
+        profile = {}
     try:
-        to_display_game_lbrary=True
-        GameLibrary= GameLibrary.objects.get(owner=user)
-        games= GameLibrary.games.all()
+        to_display_game_lbrary = True
+        GameLibrary = GameLibrary.objects.get(owner=user)
+        games = GameLibrary.games.all()
     except:
         to_display_game_lbrary=False
-        GameLibrary={}
-        games={}
-    return render(request, 'Social/profile.html',{  'to_display_profile':to_display_profile,
-                                                        'users':users,
-                                                        'display_user':user,
-                                                        'profile':profile,
-                                                        'to_display_game_lbrary':to_display_game_lbrary,
-                                                        'GameLibrary': GameLibrary,
-                                                        'games': games,
-                                                        'MEDIA_URL':settings.MEDIA_URL})
+        GameLibrary = {}
+        games = {}
+    return render(request, 'Social/profile.html',
+                  {'to_display_profile': to_display_profile,
+                   'users': users,
+                   'display_user': user,
+                   'profile': profile,
+                   'to_display_game_lbrary': to_display_game_lbrary,
+                   'GameLibrary': GameLibrary,
+                   'games': games,
+                   'MEDIA_URL': settings.MEDIA_URL})
+
 
 @login_required(login_url='/login/')
 def edit_profile(request):
@@ -58,16 +64,16 @@ def edit_profile(request):
     print(request.POST.get("biography"))
     print(request.method)
     try:
-        profile=UserProfile.objects.get(user=current_user)
+        profile = UserProfile.objects.get(user=current_user)
         print(profile)
     except ObjectDoesNotExist:
-        profile=UserProfile.objects.create(user=current_user)
-    if request.method=="POST":
-        new_profile_text=request.POST.get('biography')
+        profile = UserProfile.objects.create(user=current_user)
+    if request.method == "POST":
+        new_profile_text = request.POST.get('biography')
         print(new_profile_text)
-        profile.biography=new_profile_text
+        profile.biography = new_profile_text
         profile.save()
-    return render(request, 'Social/edit_profile.html', {'profile':profile,})
+    return render(request, 'Social/edit_profile.html', {'profile': profile, })
 
 
 @login_required(login_url='/login')
@@ -75,19 +81,21 @@ def view_event(request, event_id):
     event = Event.objects.get(pk=event_id)
     user = request.user
     user_is_allowed = user in event.participants.all()\
-                    or user == event.owner\
-                    or event.is_public
+        or user == event.owner\
+        or event.is_public
     if not user_is_allowed:
-        messages.add_message(request, messages.ERROR, gettext('You are not allowed to see this event'))
+        messages.add_message(request,
+                             messages.ERROR,
+                             gettext('You are not allowed to see this event'))
         return render(request, 'SelectGame/index.html')
-    if request.method=='POST':
-        message=UserMessage(writer=user, text=request.POST.get('message'))
+    if request.method == 'POST':
+        message = UserMessage(writer=user, text=request.POST.get('message'))
         message.save()
         event.messages.add(message)
-    users=[]
+    users = []
     users.append(event.owner)
     for participant in event.participants.all():
         users.append(participant)
-    average=rating_functions.users_rating(users, 2)
+    average = rating_functions.users_rating(users, 2)
     return render(request, 'Social/view_event.html', {'event': event,
-                                                    'averages': average})
+                                                      'averages': average})
