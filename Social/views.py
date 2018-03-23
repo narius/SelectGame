634c687b82f68a1353d9f2097bb581b7832010a4
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-23 21:01:24 +0100

    Added a view to vire private messages

--- Social/views.py
+++ Social/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import HttpResponse
 from django.contrib.auth.models import User
 from Social.models import UserProfile
 from Social.models import UserMessage
+from Social.models import PrivateMessage
 from SelectGame.models import GameLibrary
 from SelectGame.models import Event
 from SelectGame.rating import rating_functions
@@ -11,7 +12,9 @@ from django.contrib import messages
 from django.utils.translation import gettext
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.views import View
 # Create your views here.
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the social index.")
@@ -99,3 +102,37 @@ def view_event(request, event_id):
     average = rating_functions.users_rating(users, 2)
     return render(request, 'Social/view_event.html', {'event': event,
                                                       'averages': average})
+
+
+class PrivateMessageView(View):
+    '''
+        View to display private messages
+    '''
+    def get(self, request, message_id=None):
+        user = request.user
+        print(message_id)
+        if message_id is None:
+            # Get all PrivateMessages where user user is a participants
+            private_messages = PrivateMessage.objects.all()#.\
+                #filter(participants__in=[user, ])
+            print("private_messages")
+            print(private_messages)
+            return render(request, 'Social/view_privatemessages.html', {
+                "private_messages": private_messages,
+                })
+        # Get the PrivateMessge with pk=message_id,
+        # and check if user is participant
+        private_message = PrivateMessage.objects.get(pk=message_id)
+        if user in private_message.participants:
+            return render(request, 'Social/view_privatemessage.html', {
+                "private_message": private_message,
+                })
+        else:
+            messages.add_message(request,
+                                 messages.ERROR,
+                                 gettext('You are not part of this conversation'))
+            private_messages = PrivateMessage.objects.all().\
+                filter(participants__in=[user, ])
+            return render(request, 'Social/view_privatemessages.html', {
+                "private_messages": private_messages,
+                })
