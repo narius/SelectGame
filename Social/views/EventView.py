40f3f4504ea404ae130c44e46f59362917bc9cd8
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-29 13:38:17 +0200

    You can now add friends to an event

--- Social/views/EventView.py
+++ Social/views/EventView.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from Social.models import UserMessage
+from Social.models import UserProfile
 from SelectGame.models import Event
 from django.utils.translation import gettext
 from django.views import View
+from django.contrib.auth.models import User
 
 
 class EventView(View):
@@ -13,13 +15,14 @@ class EventView(View):
     def get(self, request, event_id):
         user = request.user
         event = Event.objects.get(pk=event_id)
-        profile = UserProfile.object.get(user=user)
-        friends = profile.friends
+        profile = UserProfile.objects.get(user=user)
+        friends = profile.friend_list.all()
         if not (user in event.participants.all() or user == event.owner):
             return HttpResponse(gettext("You are not part of this event"))
         return render(request,
                       'Social/view_event.html',
-                      {'event': event, })
+                      {'event': event,
+                       'friends': friends})
 
     def post(self, request, event_id):
         user = request.user
@@ -30,10 +33,19 @@ class EventView(View):
             return render(request,
                           'Social/view_event.html',
                           {'event': event, })
-        text = request.POST.get("message")
-        new_message = UserMessage(writer=user, text=text)
-        new_message.save()
-        event.messages.add(new_message)
+        if request.POST.get("new_message"):
+            text = request.POST.get("message")
+            new_message = UserMessage(writer=user, text=text)
+            new_message.save()
+            event.messages.add(new_message)
+        if request.POST.get("invite_friends"):
+            for pk in request.POST.getlist('friends'):
+                invite = User.objects.get(pk=pk)
+                event.participants.add(invite)
+            event.save()
+        profile = UserProfile.objects.get(user=user)
+        friends = profile.friend_list.all()
         return render(request,
                       'Social/view_event.html',
-                      {'event': event, })
+                      {'event': event,
+                       'friends': friends})
