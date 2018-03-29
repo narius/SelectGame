4ee33a194d275af9b53a2c184185cd3f308f0bee
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-29 23:24:24 +0200

    Shows notification on base template

--- SelectGame/views/index.py
+++ SelectGame/views/index.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from Social.models import Notification
 
 
 def index(request):
-    return render(request, 'SelectGame/index.html')
+    user = request.user
+    notifications = Notification.objects.all().\
+        filter(receiver=user).\
+        filter(status="UR")
+    return render(request,
+                  'SelectGame/index.html',
+                  {'number_of_notification': len(notifications)})
