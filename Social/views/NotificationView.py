7ba26784644f53f318ee860ec6f3048a2425cede
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-30 09:27:50 +0200

    Shows events that have issued notifcations

--- Social/views/NotificationView.py
+++ Social/views/NotificationView.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
-from Social.models import UserProfile
-from Social.models import FriendRequest
 from Social.models import Notification
+from SelectGame.models import Event
 from django.utils.translation import gettext
 from django.views import View
 from django.db.models import Q
@@ -12,11 +10,25 @@ class NotificationView(View):
 
     def get(self, request):
         user = request.user
-        notifications = Notification.objects.all().filter(receiver=user)
-        unread = notifications.filter(status="UR")
-        read = notifications.filter(status="RE")
+        user_notifications = Notification.objects.all().filter(receiver=user)
+        unread = user_notifications.filter(status="UR")
+        read = user_notifications.filter(status="RE")
         # TODO find group, event or message that created the notification
+        events = []
+        user_messages = []
+        groups = []
+        for notification in user_notifications:
+            event = notification.event.all()
+            print(event)
+            if len(event) > 0:
+                events.append(event[0])
+            user_message = notification.message.all()
+            if len(user_message) > 0:
+                user_messages.append(user_message[0])
+        print(events)
         return render(request,
                       'Social/view_notification.html',
                       {'unread': unread,
-                      'read': read})
+                       'read': read,
+                       'events': events,
+                       'user_messages': user_messages})
