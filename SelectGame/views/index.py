c0645ee881da7ba71fd90a0cb287686d00b1c7b0
Marcus Broberg <broberg.marcus@gmail.com>
2018-03-31 18:11:27 +0200

    index page for non loged in users

--- SelectGame/views/index.py
+++ SelectGame/views/index.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request,
-                  'SelectGame/index.html',
-                  {})
+    if request.user.is_anonymous:
+        return render(request,
+                      'SelectGame/index-anonymous.html',
+                      {})
+    else:
+        return render(request,
+                      'SelectGame/index.html',
+                      {})
