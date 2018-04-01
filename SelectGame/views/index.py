739a09c186d4df9043ef85115d64b2fa8318ee60
Marcus Broberg <broberg.marcus@gmail.com>
2018-04-01 21:06:22 +0200

    Shows game library

--- SelectGame/views/index.py
+++ SelectGame/views/index.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
+# SelectGame imports
+from SelectGame.models import GameLibrary
+# Social imports
+
 
 def index(request):
     if request.user.is_anonymous:
         return render(request,
                       'SelectGame/index-anonymous.html',
                       {})
-    else:
-        return render(request,
-                      'SelectGame/index.html',
-                      {})
+    user = request.user
+    game_library = GameLibrary.objects.get_or_create(owner=user)[0]
+    return render(request,
+                  'SelectGame/index.html',
+                  {'game_library': game_library})
