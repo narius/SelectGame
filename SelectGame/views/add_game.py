ba4e28db35102e413deb37efb3f4b751793628d9
Marcus Broberg <broberg.marcus@gmail.com>
2018-04-02 09:26:15 +0200

    catch if there is no image

--- SelectGame/views/add_game.py
+++ SelectGame/views/add_game.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
+from django.utils.datastructures import MultiValueDictKeyError
 from SelectGame.models import Game
 from SelectGame.models import Category
 
 
 def add_game(request):
     if request.method == 'POST':
-        print(request.FILES['image'])
+        image = None
+        try:
+            image = request.FILES[image]
+        except MultiValueDictKeyError:
+            pass
         data = {'name': request.POST.get('name'),
                 'category': request.POST.get('category'),
                 'comment': request.POST.get('comment'),
-                'image': request.FILES['image'],
+                'image': image,
                 'minimum_number_of_players':
                 request.POST.get('minimum_number_of_players'),
                 'maximum_number_of_players':
