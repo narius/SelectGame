7f553dfab45abbfecd7e3853d89085f40bbdad35
Marcus Broberg <marcus.broberg@reachmee.com>
2019-07-18 18:24:18 +0200

    removes duplicate creation of game_rating

--- migrations/20190702_02_TmK2Z-creates-game-game-rating-roles-roles-users.py
+++ migrations/20190702_02_TmK2Z-creates-game-game-rating-roles-roles-users.py
@@ -20,24 +20,6 @@ CREATE TABLE "public"."game" (
     "categories" text NOT NULL,
     PRIMARY KEY ("id")
 );
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
-
--- Sequence and defined type
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
-
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS game_rating_id_seq;
-
--- Table Definition
-CREATE TABLE "public"."game_rating" (
-    "id" int4 NOT NULL DEFAULT nextval('game_rating_id_seq'::regclass),
-    "user_id" int4 NOT NULL,
-    "rating" int4 NOT NULL DEFAULT 0,
-    "game_id" int4 NOT NULL,
-    CONSTRAINT "game_rating_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE,
-    CONSTRAINT "game_rating_game_id_fkey" FOREIGN KEY ("game_id") REFERENCES "public"."game"("id"),
-    PRIMARY KEY ("id")
-);
 
 -- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
